b9c965b97435981855e29d700c85273a83f1122e
shokkunrf <19404989+shokkunrf@users.noreply.github.com>
2024-11-01 17:16:41 +0000

    Add category name and timer in result page

--- app/src/routes/result/+page.svelte
+++ app/src/routes/result/+page.svelte
@@ -12,15 +12,32 @@
 	function next() {
 		goto('/');
 	}
+
+	let timer = 0;
+	setInterval(() => {
+		timer++;
+	}, 1000);
 </script>
 
 <div class="result">
 	<div class="contents">
-		{#if !isDisplayed}
-			議論開始！
-		{:else}
+		<div class="categoryName">
+			カテゴリ:
 			<div>
-				カテゴリ: {$categoryName}
+				{$categoryName}
+			</div>
+		</div>
+		<div>
+			{#if !isDisplayed}
+				<div class="meeting">
+					議論開始！
+					<div class="timer">
+						{Math.floor(timer / 60)
+							.toString()
+							.padStart(2, '0')}:{(timer % 60).toString().padStart(2, '0')}
+					</div>
+				</div>
+			{:else}
 				<ul>
 					{#each $words as genre, i}
 						<li>
@@ -28,8 +45,8 @@
 						</li>
 					{/each}
 				</ul>
-			</div>
-		{/if}
+			{/if}
+		</div>
 	</div>
 
 	<div class="button">
@@ -46,13 +63,28 @@
 		font-size: 2rem;
 	}
 	.contents {
-		display: flex;
-		justify-content: center;
 		height: 50vh;
+		.categoryName {
+			font-size: 0.5em;
+			background-color: #333;
+			width: 90%;
+			border-radius: 0.5em;
+			padding: 0.5em;
+			margin: 0 auto 2rem;
+			> div {
+				font-size: 1.5em;
+			}
+		}
+
+		.meeting {
+			text-align: center;
+		}
+		.timer {
+			font-size: 3rem;
+		}
 	}
 	.button {
-		display: flex;
-		justify-content: center;
+		text-align: center;
 		> button {
 			background-color: #05a;
 			border: 0 solid #000;
