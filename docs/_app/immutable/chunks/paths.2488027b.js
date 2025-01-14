9f6cbbc091e4680ecdb92380d76cba94dfac6238
shokkunrf <19404989+shokkunrf@users.noreply.github.com>
2025-01-14 13:19:13 +0900

    build for gh-pages

---
+++ docs/_app/immutable/chunks/paths.2488027b.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_p3hlm8)==null?void 0:s.base)??"/word-wolf";var a;const l=((a=globalThis.__sveltekit_p3hlm8)==null?void 0:a.assets)??e;export{l as a,e as b};
--- docs/_app/immutable/chunks/paths.e62792fa.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_4n6f8w)==null?void 0:s.base)??"/word-wolf";var a;const t=((a=globalThis.__sveltekit_4n6f8w)==null?void 0:a.assets)??e;export{t as a,e as b};
